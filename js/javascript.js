00f9396299e73cd4b0b99f3bd84d8d0e79558079
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-06-18 18:10:31 +1200

    Add functionality to buttons

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+const buttonContainer = document.querySelector('#buttonContainer')
+
+buttonContainer.addEventListener('click', (event) => {
+    if (event.target.type == 'button') {
+        let sound = event.target.dataset.sound
+        playSound(sound)
+    }
+})
+
+function playSound(sound) {
+    const audio = document.querySelector(`audio[data-sound='${sound}']`)
+    audio.currentTime = 0
+    audio.play()
+}
